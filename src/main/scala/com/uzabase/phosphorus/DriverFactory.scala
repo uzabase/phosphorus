eb96077d077b01e832364b21c17aee041c739301
Takayuki Hayashi <tkbyth@gmail.com>
2013-12-05 15:36:30 +0900

    firefox

--- src/main/scala/com/uzabase/phosphorus/DriverFactory.scala
+++ src/main/scala/com/uzabase/phosphorus/DriverFactory.scala
@@ -6,21 +6,29 @@ import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.remote.RemoteWebDriver
+import org.openqa.selenium.Platform
+import java.net.URL
 
 class DriverFactory {
-	def create:WebDriver = {
-		if(Config().isChrome) {
+	def create: WebDriver = {
+		if (Config().isChrome) {
 			System.setProperty("webdriver.chrome.driver", Config().chromeDriverUrl)
 			val options = new ChromeOptions
-			if(Config().isLang)
-				options.addArguments("--lang="+Config().lang);
+			if (Config().isLang)
+				options.addArguments("--lang=" + Config().lang);
 			return new ChromeDriver(options)
 		}
-		val capabilities = new DesiredCapabilities();
-		capabilities.setJavascriptEnabled(true);
-		val firefoxDriver = new FirefoxDriver(capabilities)
-		firefoxDriver.asInstanceOf[RemoteWebDriver].getCapabilities()
-		return firefoxDriver
+		//		val capabilities = new DesiredCapabilities();
+		//		capabilities.setJavascriptEnabled(true);
+		//		val firefoxDriver = new FirefoxDriver(capabilities)
+		//		firefoxDriver.asInstanceOf[RemoteWebDriver].getCapabilities()
+		val capability = DesiredCapabilities.firefox();
+		capability.setCapability("platform", Platform.ANY);
+		capability.setCapability("binary", "/ms/dist/fsf/PROJ/firefox/16.0.0/bin/firefox"); //for linux
+
+		//capability.setCapability("binary", "C:\\Program Files\\Mozilla  Firefox\\msfirefox.exe"); //for windows                
+		val currentDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+		return currentDriver
 	}
 }
 object DriverFactory {
