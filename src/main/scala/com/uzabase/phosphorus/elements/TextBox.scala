9587b330efcbe212f40c8585813f03ec5e4b18e0
Takayuki Hayashi <tkbyth@gmail.com>
2013-12-10 11:32:40 +0900

    text wait追加

--- src/main/scala/com/uzabase/phosphorus/elements/TextBox.scala
+++ src/main/scala/com/uzabase/phosphorus/elements/TextBox.scala
@@ -2,6 +2,7 @@ package com.uzabase.phosphorus.elements
 
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
+import org.openqa.selenium.support.ui.WebDriverWait
 
 class TextBox(val webElement:WebElement) extends Item with HasValue{
 	def appending(text:String) = webElement.sendKeys(text)
@@ -13,8 +14,12 @@ class TextBox(val webElement:WebElement) extends Item with HasValue{
 
 object TextBox extends HasUntil with InputXpath{
 	val typeName = "text"
-	def apply(atr: Predicate)(implicit driver:WebDriver) = new TextBox(driver.findElement(path(atr)))
+	def apply(atr: Predicate)(implicit driver:WebDriver):TextBox = new TextBox(driver.findElement(path(atr)))
 	def apply(element:WebElement) = new TextBox(element)
+	def apply(by:Predicate,wait:WebDriverWait)(implicit driver:WebDriver):TextBox = {
+		until(wait, path(by))
+		apply(by)
+	}
 }
 
 class Password(element: WebElement) extends TextBox(element)
@@ -22,4 +27,8 @@ object Password extends HasUntil with InputXpath{
 	val typeName = "password"
 	def apply(atr: Predicate)(implicit driver:WebDriver) = new Password(driver.findElement(path(atr)))
 	def apply(element:WebElement) = new Password(element)
+	def apply(by:Predicate,wait:WebDriverWait)(implicit driver:WebDriver):Password = {
+		until(wait, path(by))
+		apply(by)
+	}
 }
