990b587340156c13bb3b5b6a433f1b37bb996dfa
Takayuki Hayashi <tkbyth@gmail.com>
2013-12-19 01:14:43 +0900

    add test

--- src/main/scala/com/uzabase/phosphorus/Config.scala
+++ src/main/scala/com/uzabase/phosphorus/Config.scala
@@ -3,27 +3,34 @@ package com.uzabase.phosphorus
 import java.util.Properties
 
 class Config(val properties: Properties) {
-	def applicationUrl = properties.getProperty("application.url")
-	def chromeDriverUrl = properties.getProperty("chrome.driver.url")
-	def chromeBinary = properties.getProperty("chrome.binary")
-	def lang = properties.getProperty("browser.lang")
-	def isChrome = chromeDriverUrl != null && !chromeDriverUrl.isEmpty
-	def isLang = lang != null && !lang.isEmpty
-	def isRemote = {
-		val remote = properties.getProperty("remote")
-		if (remote == null || remote.isEmpty() || remote.toLowerCase() == "false")
-			false
-		else
-			true
-	}
+  def applicationUrl = properties.getProperty("application.url")
+  def chromeDriverUrl = properties.getProperty("chrome.driver.url")
+  def chromeBinary = properties.getProperty("chrome.binary")
+  def lang = properties.getProperty("browser.lang")
+  def isChrome = chromeDriverUrl != null && !chromeDriverUrl.isEmpty
+  def isLang = lang != null && !lang.isEmpty
+  def isRemote = {
+    val remote = properties.getProperty("remote")
+    if (remote == null || remote.isEmpty() || remote.toLowerCase() == "false")
+      false
+    else
+      true
+  }
 }
 
 object Config {
-	def apply() = {
-		val fileName = if (getProperty != null) getProperty else "uat.properties"
-		val properties = new Properties
-		properties.load(Thread.currentThread.getContextClassLoader.getResourceAsStream(fileName))
-		new Config(properties)
-	}
-	private def getProperty = System.getProperty("uat.property.file.name")
+
+  private lazy val defaultFile = System.getProperty("uat.property.file.name")
+  private val properties = new Properties
+
+  def apply() = getProp match {
+    case Some(s) => create(s)
+    case None => create("uat.properties")
+  }
+  private def create(fileName: String) = {
+    properties.load(Thread.currentThread.getContextClassLoader.getResourceAsStream(fileName))
+    new Config(properties)
+  }
+
+  private def getProp: Option[String] = if (defaultFile != null && !defaultFile.isEmpty) Some(defaultFile) else None
 }
--- src/test/scala/com/uzabase/phosphorus/ConfigSpec.scala
+++ src/test/scala/com/uzabase/phosphorus/ConfigSpec.scala
@@ -2,15 +2,21 @@ package com.uzabase.phosphorus
 
 class ConfigSpec extends UnitSpecification {
 
-	"設定情報" should {
-		"テスト対象となるアプリケーションのURLはapplication.urlがプロパティーファイルのキーとなる" in {
-			Config().applicationUrl must beEqualTo("http://localhost:8080/myproject")
-		}
-		"chromeDriverの設定がされている" in {
-			Config().isChrome must beTrue
-		}
-		"chromeのDriverはchrome.driver.urlがプロパティーファイルのキーとなる" in {
-			Config().chromeDriverUrl must beEqualTo("/opt/google/chrome/chromedriver")
-		}
-	}
+  "設定情報" should {
+    "テスト対象となるアプリケーションのURLはapplication.urlがプロパティーファイルのキーとなる" in {
+      Config().applicationUrl must beEqualTo("http://localhost:8080/myproject")
+    }
+    "chromeDriverの設定がされている" in {
+      Config().isChrome must beTrue
+    }
+    "chromeのDriverはchrome.driver.urlがプロパティーファイルのキーとなる" in {
+      Config().chromeDriverUrl must beEqualTo("/opt/google/chrome/chromedriver")
+    }
+    "chromeの実行ファイルはchrome.binaryがプロパティーファイルのキーとなる" in {
+      Config().chromeBinary must beEqualTo("/opt/google/chrome/chrome")
+    }
+    "Selenium Gridを使用はremoteがプロパティーファイルのキーとなる" in {
+      Config().isRemote must beTrue
+    }
+  }
 }
