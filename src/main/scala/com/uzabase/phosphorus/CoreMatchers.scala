d69bca9c5221f697f1e658278f6be6d3333f756c
Takayuki Hayashi <tkbyth@gmail.com>
2013-12-18 10:14:17 +0900

    exist処理追加

--- src/main/scala/com/uzabase/phosphorus/CoreMatchers.scala
+++ src/main/scala/com/uzabase/phosphorus/CoreMatchers.scala
@@ -4,10 +4,26 @@ import org.specs2.matcher.Matcher
 import org.specs2.matcher.Expectable
 import com.uzabase.phosphorus.elements.DropDown
 import com.uzabase.phosphorus.elements.Displayable
+import com.uzabase.phosphorus.elements.Item
+import org.openqa.selenium.NoSuchElementException
+import scala.util.control.NonFatal
 
 trait CoreMatchers {
 	def beDisplayed = new ElementMatcher((element:Displayable)=>element.displayed,s"element are displayed. ",s"element are not displayed. ")
 	def haveOption(options:String*) = new DropDownMatcher(options)
+	def notExist = new ExistMatcher
+}
+
+class ExistMatcher extends Matcher[Item] {
+	def apply[E <: Item](expectable: Expectable[E]) = {
+		try {
+			val item = expectable.value
+			result(false,"ok","expected to not exists, but exists. ",expectable)
+		}catch {
+			case e: NoSuchElementException => result(true,"ok","",expectable)
+			case NonFatal(e) => result(false,"ok",s" Oops ! ${e.getStackTrace()}",expectable)
+		}
+	}
 }
 
 class ElementMatcher(val result:(Displayable)=> Boolean,ok: String, ko: String) extends Matcher[Displayable]{
