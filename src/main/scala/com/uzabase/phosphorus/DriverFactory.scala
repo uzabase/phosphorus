36a7f69d5682228686bc1ec829cd652471a29be9
Takayuki Hayashi <tkbyth@gmail.com>
2013-12-19 09:43:46 +0900

    ie 追加

--- src/main/scala/com/uzabase/phosphorus/DriverFactory.scala
+++ src/main/scala/com/uzabase/phosphorus/DriverFactory.scala
@@ -7,34 +7,39 @@ import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.remote.RemoteWebDriver
 import java.net.URL
+import org.openqa.selenium.ie.InternetExplorerDriver
 
 class DriverFactory {
-  def create: WebDriver = {
-    if (Config().isChrome) {
-      System.setProperty("webdriver.chrome.driver", Config().chromeDriverUrl)
-      val options = new ChromeOptions
-      if (Config().isLang)
-        options.addArguments("--lang=" + Config().lang)
-      if (Config().isRemote)
-        return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeCapabilities(options));
-      return new ChromeDriver(options)
-    }
-    if (Config().isRemote) {
-      val capability = DesiredCapabilities.firefox();
-      new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
-    } else {
-      new FirefoxDriver
-    }
-  }
+	def create: WebDriver = {
+		if (Config().isChrome) {
+			System.setProperty("webdriver.chrome.driver", Config().chromeDriverUrl)
+			val options = new ChromeOptions
+			if (Config().isLang)
+				options.addArguments("--lang=" + Config().lang)
+			if (Config().isRemote)
+				return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeCapabilities(options));
+			return new ChromeDriver(options)
+		} else if (Config().isIE) {
+			System.setProperty("webdriver.ie.driver", Config().ieDriverUrl);
+			return new InternetExplorerDriver
+		} else {
+			if (Config().isRemote) {
+				val capability = DesiredCapabilities.firefox();
+				new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+			} else {
+				new FirefoxDriver
+			}
+		}
+	}
 
-  private def chromeCapabilities(options: ChromeOptions): DesiredCapabilities = {
-    val capability = DesiredCapabilities.chrome()
-    capability.setCapability(ChromeOptions.CAPABILITY, options)
-    capability.setCapability("webdriver.chrome.driver", Config().chromeDriverUrl)
-    capability.setCapability("chrome.binary", Config().chromeBinary)
-    capability
-  }
+	private def chromeCapabilities(options: ChromeOptions): DesiredCapabilities = {
+		val capability = DesiredCapabilities.chrome()
+		capability.setCapability(ChromeOptions.CAPABILITY, options)
+		capability.setCapability("webdriver.chrome.driver", Config().chromeDriverUrl)
+		capability.setCapability("chrome.binary", Config().chromeBinary)
+		capability
+	}
 }
 object DriverFactory {
-  def apply() = new DriverFactory
+	def apply() = new DriverFactory
 }
