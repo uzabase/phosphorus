9570c16152e4066037a01b4f28a565be97bde9a5
UbMac <ubmac@UbMacs-MacBook-Pro.local>
2014-10-02 18:51:54 +0900

    escape text

--- src/main/scala/com/uzabase/phosphorus/elements/Predicate.scala
+++ src/main/scala/com/uzabase/phosphorus/elements/Predicate.scala
@@ -11,7 +11,7 @@ trait ElementSelectable extends Predicate
 trait Attributable extends Predicate 
 case class id(value:String) extends Attributable {def syntax = s"contains(@id,'${value}')"}
 /** textがspecs2にいるのでアンダースコア付きにしてます。タイポじゃないよ！ */
-case class text_(value:String) extends ElementSelectable {def syntax = s"text()='${value}'"}
+case class text_(value:String) extends ElementSelectable {def syntax = s"""text()="${value}""""}
 case class class_(value:String) extends ElementSelectable {def syntax = s"@class='${value}'"}
 case class name(value:String) extends Attributable {def syntax = s"@name='${value}'"}
 case class value_(value:String) extends ElementSelectable {def syntax = s"@value='${value}'"}
