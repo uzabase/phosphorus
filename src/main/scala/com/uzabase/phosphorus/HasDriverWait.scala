3aba820d578191e2b8af0095e2ab70d02b5d845a
Takayuki Hayashi <tkbyth@gmail.com>
2016-02-18 21:19:55 +0900

    wait

--- src/main/scala/com/uzabase/phosphorus/HasDriverWait.scala
+++ src/main/scala/com/uzabase/phosphorus/HasDriverWait.scala
@@ -6,5 +6,6 @@ import org.openqa.selenium.support.ui.ExpectedCondition
 
 trait HasDriverWait {
 	def driverWait(secounds:Int=1)(implicit driver:WebDriver) = new WebDriverWait(driver,secounds)
+  def driverWait(implicit driver:WebDriver) = new WebDriverWait(driver,Config().waitTime)
 	def wait[T](f: => T, secounds:Int=1)(implicit driver: WebDriver) = new WebDriverWait(driver,secounds).until(new ExpectedCondition[T](){def apply(webDriver:WebDriver) = f})
 }
